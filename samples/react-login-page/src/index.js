911572482ee9199a705e17b9637b3770f593b8b9
djlee <djlee@kslab.co.kr>
2022-11-30 20:12:40 +0900

    Add react module

---
+++ samples/react-login-page/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Form from './Form'
+
+const inputs = [{
+  name: "username",
+  placeholder: "username",
+  type: "text"
+},{
+  name: "password",
+  placeholder: "password",
+  type: "password"
+},{
+  type: "submit",
+  value: "Submit",
+  className: "btn" 
+}]
+
+// const props = {name: 'loginForm', method: 'POST', action: 'http://localhost:9000/perform_login', inputs: inputs}
+// const props = {name: 'loginForm', method: 'POST', action: 'http://localhost:9000/login', inputs: inputs}
+const props = {name: 'loginForm', method: 'POST', action: 'http://localhost:9000/login', inputs: inputs}
+// const props = {name: 'loginForm', method: 'POST', action: 'perform_login', inputs: inputs}
+
+const params = new URLSearchParams(window.location.search)
+
+ReactDOM.render(<Form {...props} error={params.get('error')} />, document.getElementById('container'))
+
---
+++ samples/react-login-page/src/setupProxy.js
@@ -0,0 +1,4 @@
+// import { createProxyMiddleware } from 'http-proxy-middleware';
+
+// app.use('/login', createProxyMiddleware({ target: 'http://localhost:3100', changeOrigin: true }));
+// // app.use(createProxyMiddleware('/login', { target: 'http://localhost:9000', changeOrigin: true }));
